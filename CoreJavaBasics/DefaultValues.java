abccab4ee59fdeee628bfe1ce4c8ab6cd20e2067
pradeepdeiva <pradeepkumar.p@thirdware.com>
2020-11-19 19:19:52 +0530

    CoreJavaBasics

---
+++ CoreJavaBasics/DefaultValues.java
@@ -0,0 +1,24 @@
+package com.thirdware.corejavabasics;
+
+public class DefaultValues {
+
+	static byte a;
+	static short b;
+	static int c;
+	static long d;
+	static char e;
+	static boolean f;
+	static float g;
+	static double h;
+	public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+	}
+
+}
