2269a517058590e9f47d2e72f44d16cd89146ee8
pradeepdeiva <74526991+pradeepdeiva@users.noreply.github.com>
2020-11-23 22:03:30 +0530

    Update SampleRunner.java

--- CoreJavaBasics/stringconcept/SampleRunner.java
+++ CoreJavaBasics/stringconcept/SampleRunner.java
@@ -3,13 +3,22 @@ package com.thirdware.stringconcept;
 public class SampleRunner {
 
 	public static void main(String[] args) {
-		Sample sample=new Sample("Welcome","welcome");
-		sample.charAt(1);
-		sample.charArray();
-		sample.equalsMethod();
-		sample.equalsCaseMethod();
-		sample.concatMethod();
-		sample.demo();
+		String s="Welcome";
+		String s1="welcome";
+		System.out.println(s.charAt(3));
+		char ch[]=s.toCharArray();
+		for(int i=0;i<ch.length;i++) {
+			System.out.print(ch[i]+" ");
+		}
+		System.out.println((s.equals(s1)?"Strings are Equals":"Strings are not Equal"));
+		System.out.println((s.equalsIgnoreCase(s1)?"Strings are Equals":"Strings are not Equal"));
+		System.out.println(s.concat(s1));
+		System.out.println(s.isEmpty());
+		System.out.println(s.indexOf('c'));
+		System.out.println(s.replaceAll(s,"Hello"));
+		System.out.println(s.toLowerCase());
+		System.out.println(s.toUpperCase());
+		
 		
 
 	}
